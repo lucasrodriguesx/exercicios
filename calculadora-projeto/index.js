bf75d2a2fb7eaf0e0652cedf320a530518ebf787
lucasrodriguesx <gustavodrdr19@gmail.com>
2025-03-11 21:36:01 -0300

    oiaaiprof

--- calculadora-projeto/index.js
+++ calculadora-projeto/index.js
@@ -1,10 +1,6 @@
-import moment from 'moment';
+import { soma, subtracao, multiplicacao, divisao } from './calculadora.js';
 
-function calcularIdade(anoNascimento) {
-  const anoAtual = moment().year();
-  return anoAtual - anoNascimento;
-}
-
-const anoNascimento = 2003;
-const idade = calcularIdade(anoNascimento);
-console.log(`Idade: ${idade} anos`);
+console.log("Soma: ", soma(10, 5));
+console.log("Subtração: ", subtracao(10, 5));
+console.log("Multiplicação: ", multiplicacao(10, 5));
+console.log("Divisão: ", divisao(10, 5));
